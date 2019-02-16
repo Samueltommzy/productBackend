29dcb9767c310b03dac6d342cfc82c5edbabb8e6
Samuel <samueloluwatommzy@gmail.com>
2019-02-16 16:39:37 +0100

    Product Schema definition

--- model/productSchema.js
+++ model/productSchema.js
@@ -2,13 +2,13 @@ let mongoose = require('mongoose');
 let Schema   = mongoose.Schema;
 
 let Product = new Schema({
-    id:          { type:Schema.Types.ObjectId, required:true},
+    id:          { type:Schema.Types.ObjectId},
     Name:        { type:String, required:true},
     Description: { type:String, required:true},
     Price:       { type:String, required:true},
     Category:    { type:String, required:true},
-    image:       { type:String, required:true},
-    color:       { type:String, required:true}
+    Image:       { type:String, required:true},
+    Color:       { type:String, required:true}
 })
 
 module.exports = mongoose.model("Product",Product)
